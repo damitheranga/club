09d677e8d4076f6901915fdaedf28190ae3ac82e
SadeshHerath <anuradaherathkv@gmail.com>
2019-09-22 14:16:27 +0530

    v2

--- app/src/main/java/com/example/theclubd/activity/DashboardActivity.java
+++ app/src/main/java/com/example/theclubd/activity/DashboardActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.theclubd.R;
+
 public class DashboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_dashboard2);
     }
 }
